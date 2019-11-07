1ec095355930f5adc389eacbf9066c8abc9b3b58
Huangnli <50835276+Huangnli@users.noreply.github.com>
2019-11-07 11:24:40 -0400

    Update Criptografia.c

--- Criptografia.c
+++ Criptografia.c
@@ -4,15 +4,12 @@
 #include <time.h>
 
 int mdc(int num1, int num2) {
-
     int resto;
 
     do {
         resto = num1 % num2;
-
         num1 = num2;
         num2 = resto;
-
     } while (resto != 0);
 
     return num1;
